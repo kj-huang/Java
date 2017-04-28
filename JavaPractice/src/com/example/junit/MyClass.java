0da10efb91802681af408014a8a7024762a550d5
mybaseball52 <mybaseball52@gmail.com>
2017-04-28 11:52:27 +0800

    Java Practice

---
+++ JavaPractice/src/com/example/junit/MyClass.java
@@ -0,0 +1,14 @@
+package com.example.junit;
+
+/**
+ * Created by Kevin on 2017/4/28.
+ */
+public class MyClass {
+    public String sayHello(){
+        return "Hello";
+    }
+
+    public static void main(String args[]){
+
+    }
+}
---
+++ JavaPractice/tests/com/example/junit/MyClassTest.java
@@ -0,0 +1,20 @@
+package com.example.junit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kevin on 2017/4/28.
+ */
+public class MyClassTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void sayHello() throws Exception {
+    }
+
+}
